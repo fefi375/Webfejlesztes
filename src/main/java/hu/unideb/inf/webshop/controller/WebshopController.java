758de2a31ffcc0335484cb457bb39a11a5ca3fbc
Szűcs Ferenc <fefi375@gmail.com>
2024-11-21 11:12:21 +0100

    database connection

--- src/main/java/hu/unideb/inf/webshop/controller/WebshopController.java
+++ src/main/java/hu/unideb/inf/webshop/controller/WebshopController.java
@@ -1,5 +1,6 @@
 package hu.unideb.inf.webshop.controller;
 
+import hu.unideb.inf.webshop.data.entity.Felhasznalo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class WebshopController {
     @GetMapping(path = "/api")
     public String hello(){
        return "Hello World";
+    }
+
+    @GetMapping("/person")
+    public Felhasznalo szemely(){
+        return new Felhasznalo();
     }
 }
