6c7405cec15ad1451ecb411c2488e61aa06c5def
Szűcs Ferenc <fefi375@gmail.com>
2024-12-11 12:33:22 +0100

    konstruktor, getter setter

--- src/main/java/inf/unideb/beadando/data/entity/Felhasznalo.java
+++ src/main/java/inf/unideb/beadando/data/entity/Felhasznalo.java
@@ -13,4 +13,37 @@ public class Felhasznalo {
     @Column(name = "jelszo")
     private String jelszo;
 
+    public Felhasznalo() {
+
+    }
+
+    public Felhasznalo(Long id, String nev, String jelszo) {
+        this.id = id;
+        this.nev = nev;
+        this.jelszo = jelszo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public String getJelszo() {
+        return jelszo;
+    }
+
+    public void setJelszo(String jelszo) {
+        this.jelszo = jelszo;
+    }
 }
--- src/main/java/inf/unideb/beadando/data/entity/Termék.java
+++ src/main/java/inf/unideb/beadando/data/entity/Termék.java
@@ -13,4 +13,38 @@ public class Termék {
     private String nev;
     @Column(name = "ar")
     private Integer ar;
+
+    public Termék() {
+
+    }
+
+    public Termék(Long id, String nev, Integer ar) {
+        this.id = id;
+        this.nev = nev;
+        this.ar = ar;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public Integer getAr() {
+        return ar;
+    }
+
+    public void setAr(Integer ar) {
+        this.ar = ar;
+    }
 }
